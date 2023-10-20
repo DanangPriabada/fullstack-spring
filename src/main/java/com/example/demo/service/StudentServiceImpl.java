d3a79bb36201f21c1051a074d989da4473e4b6f4
Danang Priabada <danang.priabada@multipolar.com>
2023-10-20 22:35:14 +0700

    Stable Version

---
+++ src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        Iterable<Student> students = studentRepository.findAll();
+        List<Student> studentList = new ArrayList<>();
+        students.forEach(studentList::add);
+        return studentList;
+    }
+}
